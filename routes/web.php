033274a7784346c02de71ac351f5442d667b7535
kenta.koizumi <koizumi.6626@gmail.com>
2020-11-21 03:14:45 +0000

    amend top tage

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'TasksController@index');
+Route::get('/', function(){
+   return view('welcome'); 
+});
+//Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
